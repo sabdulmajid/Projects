94f00e57a93b46c26285c07341604b8de81a1a20
sabdulmajid <ayman.hasib@outlook.com>
2021-12-16 23:02:04 +0300

    Dictionary Checking Project Files

---
+++ Dictionary Manipulation/dictionary.py
@@ -0,0 +1,21 @@
+# dictionary.py
+words = set()
+
+def check(word):
+    if word in words:
+        return True
+    else:
+        return False
+
+def load(dictionary):
+    file = open(dictionary, "r")
+    for line in file:
+        words.add(line.rstrip())
+    file.close()
+    return True
+
+def size():
+    return len(words)
+
+def unload():
+    return True
